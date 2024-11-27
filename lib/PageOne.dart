523f780ddabaaa7c4d4f0807083171484b3e893b
koksal100 <baskcar1@hotmail.com>
2024-11-28 02:11:04 +0300

    adding textfield to the pageOne for getting a nickname from user

--- lib/PageOne.dart
+++ lib/PageOne.dart
@@ -17,103 +17,149 @@ class _PageOneState extends State<PageOne> {
   ];
 
   bool isRaceStarted = false;
+  final TextEditingController _controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _controller
+        .dispose(); // Bellek sızıntısını önlemek için controller'ı serbest bırakıyoruz.
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (!isRaceStarted) ...[
-            SizedBox(
-              height: 120,
-              child: Center(
-                child: PageView.builder(
-                  onPageChanged: (index) {
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (!isRaceStarted) ...[
+              SizedBox(
+                height: 120,
+                child: Center(
+                  child: PageView.builder(
+                    onPageChanged: (index) {
+                      setState(() {
+                        currentIndex = index % imagePaths.length;
+                      });
+                    },
+                    controller: PageController(viewportFraction: 0.55),
+                    itemBuilder: (context, index) {
+                      final actualIndex = index % imagePaths.length;
+                      final isSelected = actualIndex == currentIndex;
+                      return AnimatedScale(
+                        scale: isSelected ? 1.0 : 0.6,
+                        duration: const Duration(milliseconds: 300),
+                        child: AnimatedOpacity(
+                          duration: const Duration(milliseconds: 1200),
+                          opacity: isSelected ? 1.0 : 0.5,
+                          child: Image.asset(
+                            imagePaths[actualIndex],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Seçilen dil: ${languages[currentIndex]}',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 250,
+                child: TextFormField(
+                    textAlign: TextAlign.start,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Bu alan boş bırakılamaz!';
+                      } else if (value.length < 4) {
+                        return 'Nickname en az 5 karakterden oluşmalıdır';
+                      }
+                      return null;
+                    },
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      errorMaxLines: 2,
+                      errorStyle: TextStyle(
+                        fontSize: 14, // Yazı boyutunu burada ayarlayın
+                        color: Colors.red, // Hata mesajının rengini değiştirin
+                        fontWeight:
+                            FontWeight.bold, // Yazı kalınlığını ayarlayın
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 9,
+                              color: Color.fromARGB(255, 174, 133, 231)),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(24.0))),
+                      hintText: 'Nickname giriniz...',
+                    )),
+              ),
+              const SizedBox(height: 20),
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color(0xFFE0BBE4), // Açık mor
+                      Color.fromARGB(255, 144, 103, 185),
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(33.0),
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                        isRaceStarted = true;
+                      });
+                    }
+                    ;
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.deepPurple[100],
+                    elevation: 5,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 12,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "Yarışa Başla",
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+            if (isRaceStarted) ...[
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
                     setState(() {
-                      currentIndex = index % imagePaths.length;
+                      isRaceStarted = false;
                     });
                   },
-                  controller: PageController(viewportFraction: 0.55),
-                  itemBuilder: (context, index) {
-                    final actualIndex = index % imagePaths.length;
-                    final isSelected = actualIndex == currentIndex;
-                    return AnimatedScale(
-                      scale: isSelected ? 1.0 : 0.6,
-                      duration: const Duration(milliseconds: 300),
-                      child: AnimatedOpacity(
-                        duration: const Duration(milliseconds: 1200),
-                        opacity: isSelected ? 1.0 : 0.5,
-                        child: Image.asset(
-                          imagePaths[actualIndex],
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
+                  child: const Text('Yarışı Bitir'),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Seçilen dil: ${languages[currentIndex]}',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            DecoratedBox(
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [
-                    Color(0xFFE0BBE4), // Açık mor
-                    Color.fromARGB(255, 144, 103, 185),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-                borderRadius: BorderRadius.circular(33.0),
-              ),
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    isRaceStarted = true;
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  shadowColor: Colors.deepPurple[100],
-                  elevation: 5,
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24,
-                    vertical: 12,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                  ),
-                ),
-                child: const Text(
-                  "Yarışa Başla",
-                  style: TextStyle(
-                    fontSize: 23,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+            ],
           ],
-          if (isRaceStarted) ...[
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    isRaceStarted = false;
-                  });
-                },
-                child: const Text('Yarışı Bitir'),
-              ),
-            ),
-          ],
-        ],
+        ),
       ),
     );
   }
